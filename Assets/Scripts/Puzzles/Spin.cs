59bb6f9b26f1c6ebcd28bf1353ade35108b187f6
HaziqIrfan <wevilbear@mail.com>
2019-02-24 07:26:53 +0800

    left the wire and the picture

--- Assets/Scripts/Puzzles/Spin.cs
+++ Assets/Scripts/Puzzles/Spin.cs
@@ -9,10 +9,59 @@ public class Spin : MonoBehaviour
     [SerializeField]
     private Button[] SpinObject;
 
+    [SerializeField]
+    List<string> top_LeftTextList = new List<string>();//Ah Lian, Atas, Ayam right
+    [SerializeField]
+    List<string> top_RightTextList = new List<string>();//Kena, Kiasu, Kaypoh
+    [SerializeField]
+    List<string> bottom_LeftTextList = new List<string>();//Sibeh, Sian, Siam
+    [SerializeField]
+    List<string> bottom_RightTextList = new List<string>();//Abit, Act blur, Abuden
+
+    [SerializeField]
+    private Text top_LeftText;
+
+    [SerializeField]
+    private Text top_RightText;
+
+    [SerializeField]
+    private Text bottom_LeftText;
+
+    [SerializeField]
+    private Text bottom_RightText;
+
+    int topLeftTextIndex;
+    int topRightTextIndex;
+    int bottomLeftTextIndex;
+    int bottomRightTextIndex;
+
+    float topLeftRotateAngle;
+    float topRightRotateAngle;
+    float bottomLeftRotateAngle;
+    float bottomRightRotateAngle;
+
+    int topLeftAnswer;
+    int TopRightAnswer;
+    int bottomLeftAnswer;
+    int bottomRightAnswer;
+
+    float playerTopLeftAnswer;
+    float playerTopRightAnswer;
+    float playerBottomleftAnswer;
+    float playerBottomRightAnswer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        topLeftTextIndex = Random.Range(0, 2);
+        topRightTextIndex = Random.Range(0, 2);
+        bottomLeftTextIndex = Random.Range(0, 2);
+        bottomRightTextIndex = Random.Range(0, 2);
+
+        top_LeftText.text = top_LeftTextList[topLeftTextIndex];
+        top_RightText.text = top_RightTextList[topRightTextIndex];
+        bottom_LeftText.text = bottom_LeftTextList[bottomLeftTextIndex];
+        bottom_RightText.text = bottom_RightTextList[bottomRightTextIndex];
     }
 
     // Update is called once per frame
@@ -25,5 +74,28 @@ public class Spin : MonoBehaviour
     {
             SpinObject[buttonIndex].transform.Rotate(0,0,90);
             Debug.Log("button is clicked");
+    }
+
+    public void CalculateAnswer()
+    {
+        topLeftRotateAngle = 90 * (topLeftTextIndex + 1);
+        topRightRotateAngle = 90 * (topRightTextIndex + 1);
+        bottomLeftRotateAngle = 90 * (bottomLeftTextIndex + 1);
+        bottomRightRotateAngle = 90 * (bottomRightTextIndex + 1);
+
+        if (SpinObject[0].transform.eulerAngles.z == topLeftRotateAngle &&
+            SpinObject[1].transform.eulerAngles.z == topRightRotateAngle &&
+            SpinObject[2].transform.eulerAngles.z == bottomLeftRotateAngle &&
+            SpinObject[3].transform.eulerAngles.z == bottomRightRotateAngle)
+        {
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Debug.Log(topLeftRotateAngle);
+            Debug.Log(topRightRotateAngle);
+            Debug.Log(bottomLeftRotateAngle);
+            Debug.Log(bottomRightRotateAngle);
+        }
     }
 }
