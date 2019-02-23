e24bb61b7d3db34413501d806e9f61d94a2f95ef
Hui Sheng @ Laptop <deathezer10@gmail.com>
2019-02-23 22:14:34 +0800

    Initial Walkie talkie

---
+++ Assets/Scripts/MicrophoneButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class MicrophoneButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField]
+    private Sprite m_OnPressedSprite;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GetComponent<Image>().sprite = m_OnPressedSprite;
+        Microphone.Start("", false, 0, 44100);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+        Microphone.End("");
+    }
+
+}
