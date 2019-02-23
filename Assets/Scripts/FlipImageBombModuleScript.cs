260aecc953436088b8d532d9ae301875d0c9b480
Rync99 <rync99@hotmail.com>
2019-02-23 23:21:45 +0800

    >Bomb Module

---
+++ Assets/Scripts/FlipImageBombModuleScript.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlipImageBombModuleScript : MonoBehaviour
+{
+    List<string> MainTextList = new List<string>();//Ah Lian, Atas, Ayam right
+    List<string> TopTextList = new List<string>();//Kena, Kiasu, Kaypoh
+    List<string> BottomTextList = new List<string>();//Sibeh, Sian, Siam
+
+    public Text MainText;
+    public Image TopText;
+    public Image BottomText;
+
+    int maintext;
+    int toptext;
+    int bottomtext;
+
+    float TopRotateAngle;
+    float BottomRotateAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainTextList.Add("Ah Lian");
+        MainTextList.Add("Atas");
+        MainTextList.Add("Ayam");
+        TopTextList.Add("Kena");
+        TopTextList.Add("Kiasu");
+        TopTextList.Add("Kaypoh");
+        BottomTextList.Add("Sibeh");
+        BottomTextList.Add("Sian");
+        BottomTextList.Add("Siam");
+
+        maintext = Random.Range(0, 2);
+        toptext = Random.Range(0, 2);
+        bottomtext = Random.Range(0, 2);
+
+        MainText.text = MainTextList[maintext];
+        TopText.transform.GetChild(0).GetComponent<Text>().text = TopTextList[toptext];
+        BottomText.transform.GetChild(0).GetComponent<Text>().text = BottomTextList[bottomtext];
+
+        CalculateRotateAnswer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RotateTopImageLeft()
+    {
+        TopText.transform.Rotate(0, 0, 90);
+        //Debug.Log(TopText.transform.rotation.eulerAngles.z);
+    }
+    public void RotateTopImageRight()
+    {
+        TopText.transform.Rotate(0, 0, -90);
+        //Debug.Log(TopText.transform.rotation.eulerAngles.z);
+    }
+    public void RotateBottomImageLeft()
+    {
+        BottomText.transform.Rotate(0, 0, 90);
+        //Debug.Log(BottomText.transform.rotation.eulerAngles.z);
+    }
+    public void RotateBottomImageRight()
+    {
+        BottomText.transform.Rotate(0, 0, -90);
+        //Debug.Log(BottomText.transform.rotation.eulerAngles.z);
+    }
+
+    void CalculateRotateAnswer()
+    {
+        int top = 0;
+        int bottom = 0;
+        if(maintext == 0)
+        {
+            top = toptext - 2 + 1;
+            bottom = bottomtext - 1 + 1;
+        }
+        else if(maintext == 1)
+        {
+            top = toptext - 1 + 1;
+            bottom = bottomtext - 2 + 1;
+        }
+
+        if (top < 0)
+            TopRotateAngle = 360 + (top * 90);
+        else
+            TopRotateAngle = top * 90;
+
+        if (bottom < 0)
+            BottomRotateAngle = 360 + (bottom * 90);
+        else
+            BottomRotateAngle = bottom * 90;
+    }
+
+    public void ConfirmAnswer()
+    {
+        if (TopText.transform.rotation.eulerAngles.z == TopRotateAngle && BottomText.transform.rotation.eulerAngles.z == BottomRotateAngle)
+            Debug.Log("Correct");
+        else
+        {
+            Debug.Log(TopRotateAngle);
+            Debug.Log(BottomRotateAngle);
+        }
+            
+    }
+}
---
+++ Assets/Scripts/ImageBombModule.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageBombModule : MonoBehaviour
+{
+    public Button leftButton;
+    public Button middleButton;
+    public Button rightButton;
+
+    List<string> LeftTextList = new List<string>();//Abuden, Alamak, Bo Jio
+    List<string> MiddleTextList = new List<string>();//Bodoh, Encik, Chim
+    List<string> RightTextList = new List<string>();//Chope, Liao, Rabak
+    List<int> Numbers = new List<int>();
+
+    int displayLeft;
+    int displayMiddle;
+    int displayRight;
+    int answer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeftTextList.Add("Abuden");
+        LeftTextList.Add("Alamak");
+        LeftTextList.Add("Bo Jio");
+        MiddleTextList.Add("Bodoh");
+        MiddleTextList.Add("Encik");
+        MiddleTextList.Add("Chim");
+        RightTextList.Add("Chope");
+        RightTextList.Add("Liao");
+        RightTextList.Add("Rabak");
+        Numbers.Add(0);
+        Numbers.Add(1);
+        Numbers.Add(2);
+
+        displayLeft = Random.Range(0, 2);
+        RemoveFromNumberList(displayLeft);
+        displayMiddle = 
+        leftButton.transform.GetChild(0).GetComponent<Text>().text = LeftTextList[displayLeft];
+        middleButton.transform.GetChild(0).GetComponent<Text>().text = MiddleTextList[displayMiddle];
+        rightButton.transform.GetChild(0).GetComponent<Text>().text = RightTextList[displayRight];
+    }
+
+    void RemoveFromNumberList(int number)
+    {
+        for(int a = 0; a < Numbers.Count; a++)
+        {
+            if (number == Numbers[a])
+            {
+                Numbers.RemoveAt(a);
+                break;
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
