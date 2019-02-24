37e1118d9ce233bc87b3d1ea1cf095fc9fcc0671
Rync99 <rync99@hotmail.com>
2019-02-24 09:24:16 +0800

    >Player Control start

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour
+{
+    //public Sprite Cross;
+    //public Image First_Box;
+    //public Image Second_Box;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //First_Box.sprite = Cross;
+        //Second_Box.sprite = Cross;
+        //First_Box.enabled = false;
+        //Second_Box.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
